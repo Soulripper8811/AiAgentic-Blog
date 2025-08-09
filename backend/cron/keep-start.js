9b2ec59fcbf4d6c64ab22f35bd04d903b6a20b65
Soulrippper8811 <134708634+Soulripper8811@users.noreply.github.com>
2025-08-09 18:38:02 +0530

    completed

--- backend/cron/keep-start.js
+++ backend/cron/keep-start.js
@@ -5,8 +5,8 @@ dotenv.config();
 const keepStart = () => {
   nodeCron.schedule("*/10 * * * *", async () => {
     console.log("Keep Start: Running task to keep the server alive");
-    // const response = await fetch(`${process.env.HOSTED_URL}/api/check`);
-    const response = await fetch(`http://localhost:4000/api/check`);
+    const response = await fetch(`${process.env.HOSTED_URL}/api/check`);
+    // const response = await fetch(`http://localhost:4000/api/check`);
     const data = await response.json();
     console.log("Keep Start: Response from server:", data);
   });
